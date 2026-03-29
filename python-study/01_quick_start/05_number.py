749d388ece59604b44cc2a7ceb4ca94b620041a5
wangguoxi <wangguoxi@darryring.com>
2026-03-29 17:29:35 +0800

    feat:补充数字类型内容

--- python-study/01_quick_start/05_number.py
+++ python-study/01_quick_start/05_number.py
@@ -5,12 +5,44 @@
 # 复数：用来表示复数，如1+2j,3+4j,5+6j,7+8j,9+10j
 
 # 整数类型：int
-# python中只有int一种证书类型，没有long类型
+# python3中只有int一种证书类型，不再有python2的long类型
 print('整数类型：int')
 i = 1
 print('i:', i)
 k = -100
 print('k:', k)
+
+# 算术运算
+# 加法
+print('i + k = ', i + k)
+# 减法
+print('i - k = ', i - k)
+# 乘法
+print('i * k = ', i * k)
+# 除法(返回浮点数float)
+print('i / k = ', i / k)
+x = 9
+y = 3
+num = x / y
+print('x / y = ', num)
+print('type(num) = ', type(num))
+# 取整除(向下取整,得到整数int)
+j = 7
+k = 3
+print('(1)j // k = ', j // k)
+i = -7
+num = i // k
+print('(2)i // k = ', num)
+print('type(num) = ', type(num))
+# 取模(取余数)
+a = 3
+b = 7
+print('a % b = ', a % b)
+# 幂
+m = 2
+n = 3
+print('m ** n = ', m ** n)
+
 
 print('==============================')
 print('浮点数类型：float')
@@ -27,7 +59,8 @@ print('f4:', f4)
 
 print('==============================')
 print('复数类型：complex')
-# 复数类型：complex
+# 复数类型：complex，支持加减乘除
+# 格式： a + bj,其中a是实部，b是虚部，j是虚数单位, a和b都是浮点数float
 c = 1.23j
 print('c:', c)
 c2 = 1.23e3j
@@ -38,3 +71,13 @@ c4 = -1.23j
 print('c4:', c4)
 c5 = c + c2
 print('c5:', c5)
+
+# 使用complex函数创建复数 1 + 2j
+c6 = complex(1, 2)
+print('c6:', c6)
+
+# 获取复数的实部和虚部
+print('c6.real = ', c6.real)
+print('c6.imag = ', c6.imag)
+print('type(c6.real) = ', type(c6.real))
+print('type(c6.imag) = ', type(c6.imag))
