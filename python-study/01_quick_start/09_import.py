80b4bc4a35499ebfde6637a35e4439a6677b32d3
wangguoxi <wangguoxi@darryring.com>
2026-03-29 16:03:51 +0800

    feat:09_import补充别名和from...import..

--- python-study/01_quick_start/09_import.py
+++ python-study/01_quick_start/09_import.py
@@ -5,3 +5,16 @@
 import sys
 # 打印python文件搜索的路径
 print("sys.path:", sys.path)
+
+# 只导入模块的某个函数
+from random import randint
+print("生成随机数1：", randint(1, 100))
+
+# 为导入模块起别名
+# 起别名作用：
+# 1. 简化模块名，尤其对于一些模块名很长的模块，能大大降低代码量,提高可读性
+# 2. 解决冲突：当从不同模块导入的函数名相同时，可以起别名避免冲突
+from os import path as p
+from sys import path as s
+print('os.path:', p)
+print('sys.path:', s)
