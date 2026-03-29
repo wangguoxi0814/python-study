ec651ebffe4708bf46b1f7390ad6376687eb6278
wangguoxi <wangguoxi@darryring.com>
2026-03-29 15:39:02 +0800

    feat:08_print入门

---
+++ python-study/08_print.py
@@ -0,0 +1,9 @@
+# print的使用，print即在运行时将内容打印到终端
+
+# print()函数，用于将内容打印到终端,默认打印后换行
+print('Hello Python!')
+print('Hello World!')
+
+#不换行输出
+print('Hello Python!', end= '')
+print('Hello World!', end = '')
