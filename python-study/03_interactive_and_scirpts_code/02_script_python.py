7873f8f203e210b3f2cf31ba0605b5cbdf745299
wangguoxi <wangguoxi@darryring.com>
2026-04-14 21:43:30 +0800

    feat(script_python): add script_python.py 说明python脚本式编程

---
+++ python-study/03_interactive_and_scirpts_code/02_script_python.py
@@ -0,0 +1,7 @@
+
+print("Hello, Script Python!")
+
+# 脚本编程,在linux系统中，使用#!/usr/bin/env python 来表示脚本编程,如此文件第一行，这样在linux/Unix/macOS系统中，通过./<文件名>即可运行
+# 脚本编程是指将代码保存到文件中，然后通过Python解释器执行文件中的代码
+# 脚本编程的优点：可以保存代码，方便后续使用
+
