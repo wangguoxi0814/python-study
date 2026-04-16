622c6c700394797575af4a267fb4a9406d726445
wangguoxi <wanggx@epfost.com.cn>
2026-04-16 13:09:07 +0800

    feat(cond_control):增加条件控制demo

---
+++ python-study/05_flow_control/01_cond_control.py
@@ -0,0 +1,25 @@
+# 条件控制语句
+
+condition = True
+if condition:
+    print("condition is true")
+else:
+    print("condition is false")
+
+condition2 = False
+if condition2:
+    print("condition is true")
+elif condition2:
+    print("condition2 is true")
+else:
+    print("condition is false")
+
+# pass 留空占位，当无逻辑需要处理时，置空会包括，需要用pass占位
+cond = True
+if cond: 
+    pass
+
+
+
+
+
