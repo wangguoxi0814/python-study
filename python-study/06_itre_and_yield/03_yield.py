56025d545a00bcf3740b65c95745ff1708f89be7
wangguoxi <wanggx@epfost.com.cn>
2026-04-21 16:47:05 +0800

    feat(生成器):增加yield的demo

---
+++ python-study/06_itre_and_yield/03_yield.py
@@ -0,0 +1,55 @@
+# yield生成器
+# 生成器是一种特殊的迭代器，可以生成一个序列，每次调用next()方法时，生成器会返回一个值，直到生成器结束
+# yield有点类似DEBUG的断点，执行到yield时，会暂停，并返回一个值，下次调用next()方法时，会从上次暂停的地方继续执行
+print("=============1==============")
+def countDown(f):
+    while f >= 0: 
+        yield f
+        f -= 1
+
+count = countDown(10)
+print(count)
+# for i in count:
+#     print(i, end=' ')
+# print()
+i1 = next(count)
+print(i1)
+i2 = next(count)
+print(i2)
+i3 = next(count)
+print(i3)
+i4 = next(count)
+print(i4)
+i5 = next(count)
+print(i5)
+
+print("=============2==============")
+# 生成器可以用于生成一个无限的序列，比如斐波那契数列
+def fibonacci(n):
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n):
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+
+f = fibonacci(10)
+f1 = next(f)
+f2 = next(f)
+f3 = next(f)
+print(f1, f2, f3)
+
+for i in f:
+    print(i, end=' ')
+print()
+
+# 生成器可以用于生成一个有限的序列，比如一个列表
+print("=============3==============")
+def myGenerator():
+    yield 1
+    yield 2
+    yield 3
+g = myGenerator()
+print(next(g))
+print(next(g))
