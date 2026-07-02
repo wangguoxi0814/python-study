ac3624cd0f26066ad7bd2e8a855406c94ec82c11
wangguoxi <wanggx@epfost.com.cn>
2026-07-02 17:07:17 +0800

    fix(bool):修正拼写错误

--- python-study/02_data_type/03_bool.py
+++ python-study/02_data_type/03_bool.py
@@ -25,5 +25,5 @@ print('Ture == 1 :', True == 1)
 print('False == 0 :', False == 0)
 print('True + 1 =', True + 1)
 print('False + 1 =', False + 1)
-print('True + Flase =', True + False)
+print('True + False =', True + False)
 
