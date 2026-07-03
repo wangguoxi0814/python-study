d2704309b42375c1127c52a91227b5508cf20e71
wangguoxi <wanggx@epfost.com.cn>
2026-07-03 13:14:54 +0800

    feat(sorted): 内置函数reversed使用

--- python-study/02_data_type/12_reversed.py
+++ python-study/02_data_type/12_reversed.py
@@ -9,9 +9,11 @@
 
 
 # reversed() 反转， 不是原地反转，会返回结果为reverseiterator，即反转迭代器对象，这样不会一次性把反转结果放到内存，而是使用时，再逐个生成结果
+# 注意，迭代器只可以用一次
 # reversed操作list，会返回list_reversed_iterator，它持有list的引用，迭代效率更高
 # reversed操作tuple/str等迭代对象，统一返回reversed迭代器对象
 # reversed操作range，返回range_iterator
+# reversed操作dict，返回dict_reversekeyiterator对象,只对key做反转
 rl = [1, 2, 3, 4, 5]
 reversed_ite = reversed(rl)
 reversed_list = list(reversed_ite)
@@ -28,11 +30,27 @@ print()
 # print('reversed_set is:', reversed_set)
 # print()
 
+# dict
+d1 = {1: 'beijing', 2: 'shanghai', 3: 'shenzhen'}
+s_d1 = reversed(d1)
+print('s_d1 is:', s_d1)  # dict_reversekeyiterator
+ks = set(s_d1)
+print('ks is:', ks)
+# 由于迭代器只可以用一次，下面再次使用迭代器转为list，会得到空列表,但如果你需要得到一个list，通过list构造器和key的迭代器是可以的，但要保证迭代器可用
+kl = list(s_d1)
+print('kl is:', kl)
+
+d2 = {'beijing': 1, 'shanghai': 2, 'shenzhen': 3}
+r_ite_d2 = reversed(d2)
+r_d2_list = list(r_ite_d2)
+print('r_d2_list is:', r_d2_list)
+print()
+
 rt = (1, 2, 3, 4, 5)
 reversed_t_ite = reversed(rt)
 reversed_t = tuple(reversed_t_ite)
 print('rt is:', rt)
-print('reversed_t_ite is:', reversed_t_ite)   # <reversed object at 0x0000025D79C32380>
+print('reversed_t_ite is:', reversed_t_ite)  # <reversed object at 0x0000025D79C32380>
 print('reversed_t is:', reversed_t)
 print()
 
