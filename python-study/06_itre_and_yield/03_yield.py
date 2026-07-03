2558b8d8ef7de7d1a16a58e4d3051ecc6088d189
wangguoxi <wanggx@epfost.com.cn>
2026-07-03 18:00:16 +0800

    fix(yield): 斐波那列数列算法

--- python-study/06_itre_and_yield/03_yield.py
+++ python-study/06_itre_and_yield/03_yield.py
@@ -2,13 +2,16 @@
 # 生成器是一种特殊的迭代器，可以生成一个序列，每次调用next()方法时，生成器会返回一个值，直到生成器结束
 # yield有点类似DEBUG的断点，执行到yield时，会暂停，并返回一个值，下次调用next()方法时，会从上次暂停的地方继续执行
 print("=============1==============")
-def countDown(f):
-    while f >= 0: 
+
+
+def count_down(f):
+    while f >= 0:
         yield f
         f -= 1
 
-count = countDown(10)
-print(count)
+
+count = count_down(10)
+print(count)  # generator
 # for i in count:
 #     print(i, end=' ')
 # print()
@@ -24,6 +27,8 @@ i5 = next(count)
 print(i5)
 
 print("=============2==============")
+
+
 # 生成器可以用于生成一个无限的序列，比如斐波那契数列
 def fibonacci(n):
     a, b, counter = 0, 1, 0
@@ -33,6 +38,7 @@ def fibonacci(n):
         yield a
         a, b = b, a + b
         counter += 1
+
 
 f = fibonacci(10)
 f1 = next(f)
@@ -46,10 +52,26 @@ print()
 
 # 生成器可以用于生成一个有限的序列，比如一个列表
 print("=============3==============")
+
+
 def myGenerator():
     yield 1
     yield 2
     yield 3
+
+
 g = myGenerator()
 print(next(g))
 print(next(g))
+
+
+# 1 1 2 3
+def fib(num):
+    a, b = 0, 1
+    r = a + b
+
+
+
+print(f'fib num is {fib(4)}')
+print(f'fib num is {fib(5)}')
+print(f'fib num is {fib(6)}')
