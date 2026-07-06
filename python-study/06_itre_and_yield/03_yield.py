d5bd3a6612f84cb91813d2fff28ba85de4ebd118
wangguoxi <wanggx@epfost.com.cn>
2026-07-06 16:21:46 +0800

    feat:yield补充说明

--- python-study/06_itre_and_yield/03_yield.py
+++ python-study/06_itre_and_yield/03_yield.py
@@ -1,6 +1,7 @@
 # yield生成器
 # 生成器是一种特殊的迭代器，可以生成一个序列，每次调用next()方法时，生成器会返回一个值，直到生成器结束
 # yield有点类似DEBUG的断点，执行到yield时，会暂停，并返回一个值，下次调用next()方法时，会从上次暂停的地方继续执行
+# 如果yield所在方法执行结束，还调用next()会出现StopIteration异常
 print("=============1==============")
 
 
@@ -33,18 +34,19 @@ print("=============2==============")
 def fibonacci(n):
     a, b, counter = 0, 1, 0
     while True:
-        if (counter > n):
+        if (counter >= n):
             return
         yield a
         a, b = b, a + b
         counter += 1
 
 
-f = fibonacci(10)
-f1 = next(f)
-f2 = next(f)
-f3 = next(f)
-print(f1, f2, f3)
+f = fibonacci(100)
+# i = 0
+# while i<10:
+#     i = i + 1
+#     print(f'第{i}个fib number：{next(f)}')
+
 
 for i in f:
     print(i, end=' ')
@@ -65,13 +67,4 @@ print(next(g))
 print(next(g))
 
 
-# 1 1 2 3
-def fib(num):
-    a, b = 0, 1
-    r = a + b
 
-
-
-print(f'fib num is {fib(4)}')
-print(f'fib num is {fib(5)}')
-print(f'fib num is {fib(6)}')
