b8950644c0372e9bad5de1dfc7e697c7a9b083e3
wangguoxi <wangguoxi@darryring.com>
2026-07-10 22:12:29 +0800

    feat: all & any函数

---
+++ python-study/08_func/13_all&any.py
@@ -0,0 +1,10 @@
+# all() 所有元素为True,返回True，否则False
+# any() 任意元素为True，返回True，否则Flase
+
+l = ['Hello', 'Python', None]
+print(f'all:{all(l)}')
+print(f'any:{any(l)}')
+
+k = ['US', 'CN', 'BR']
+print(f'all2: {all(k)}')
+print(f'any2: {any(k)}')
