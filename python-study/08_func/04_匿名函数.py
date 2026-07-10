912392a063e21285c4e930048b74ceb14dab4877
wangguoxi <wangguoxi@darryring.com>
2026-07-10 08:58:11 +0800

    feat:匿名函数

---
+++ python-study/08_func/04_匿名函数.py
@@ -0,0 +1,26 @@
+# 匿名函数
+# 1. 格式： lambda 参数: 表达式
+# 2. 用途： 用来封装简单、一次性逻辑，提升代码简洁性
+# 3. 限制：
+#    * 所有内容必须在一行，且只能有一个表达式
+#    * 无需写return，自动将表达式的值返回
+
+def calculate(func, x, y):
+    return func(x, y)
+
+
+# 加法实现
+result1 = calculate(lambda x, y: x + y, 30, 20)
+print(f'result1={result1}')
+# 减法实现
+result2 = calculate(lambda x, y: x - y, 30, 20)
+print(f'result2={result2}')
+print('-' * 40)
+
+# 无参写法
+lam_func1 = lambda : print('无参匿名函数')
+lam_func1()
+print('-' * 40)
+
+# 多表达式，编译报错
+# lam_func2 = lambda x: x + 10, x -10
