8919d8df1325a5dfc51787d24a0656a1c7d5a93e
wangguoxi <wangguoxi@darryring.com>
2026-07-12 16:25:37 +0800

    feat: 标准模块

---
+++ python-study/09_modules/02_std_module.py
@@ -0,0 +1,17 @@
+# 标准库模块
+
+import copy
+import os
+import random
+import time
+import math
+import sys
+
+# python实现的内置模块
+print(copy.__file__)
+print(os.__file__)
+print(random.__file__)
+# C语言实现的内置模块，无法获取文件信息，只有存根文件
+# print(time.__file__)
+# print(math.__file__)
+# print(sys.__file__)
