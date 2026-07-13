1ffc970bdd0a3db6b553c32052e60e3ef6b09752
wangguoxi <wangguoxi@darryring.com>
2026-07-13 16:10:48 +0800

    feat: 生成器补充注释和send、yield from

--- python-study/06_itre_and_yield/03_yield.py
+++ python-study/06_itre_and_yield/03_yield.py
@@ -1,7 +1,10 @@
 # yield生成器
 # 生成器是一种特殊的迭代器，可以生成一个序列，每次调用next()方法时，生成器会返回一个值，直到生成器结束
 # yield有点类似DEBUG的断点，执行到yield时，会暂停，并返回一个值，下次调用next()方法时，会从上次暂停的地方继续执行
-# 如果yield所在方法执行结束，还调用next()会出现StopIteration异常
+# 一个方法里只要出现yield，它的返回值就是生成器，就可以用于遍历和直接转化为list、set等容器，但直接转如果数据量太多，有爆内存风险
+# 如果yield所在方法执行结束，还调用next()会出现StopIteration异常,或者yield执行遇到return
+# yield可以使用send传入参数，但第一次调用生成器只能传None。send相当于执行next同时传入参数
+# yield from Iterable可以替代 for循环yield，将可迭代对象元素返回
 print("=============1==============")
 
 
@@ -66,5 +69,32 @@ g = myGenerator()
 print(next(g))
 print(next(g))
 
+print('-' * 40)
 
 
+# yield from
+def yield_from():
+    nums = [1, 2, 3, 4]
+    yield from nums
+
+
+for i in yield_from():
+    print(i)
+
+# yield send
+print('-' * 40)
+
+def yield_send():
+    print('yield send start')
+    a = yield 'yield 1'
+    print(f'a: {a}')
+    b = yield 'yield 2'
+    print(f'b: {b}')
+
+y = yield_send()
+# TypeError: can't send non-None value to a just-started generator
+# y.send('heihei')
+y.send(None)
+y.send('Haha')
+# y.send('HEIHEI')
+# y.send('yuyu')
