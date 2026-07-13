88c768c975679c7ed55f2b11829a0cbe7bcdb807
wangguoxi <wangguoxi@darryring.com>
2026-07-13 10:45:16 +0800

    feat: 包安装、全局环境、虚拟环境、安装包路径

---
+++ python-study/09_modules/04_pip_cmd.py
@@ -0,0 +1,11 @@
+# pip
+# pip install
+# pip uninstall
+
+# 默认pip是操作全局环境
+# .venv创建虚拟环境
+# activate进入虚拟环境， deactivate退出
+
+# cmd有管理员权限和没有在安装包的路径不一样
+# 如果没有管理员权限，可以通过python -m site --user-site查看路径,一般C盘用户目录
+# 如果有管理员权限，一般是在解释器的site-packages下
