fd25b34f6fde260e9915566fc7cd0844cb528a8f
wangguoxi <wangguoxi@darryring.com>
2026-07-30 17:56:12 +0800

    docs(python-study): 更新import模块导入说明文档
    - 添加了关于sys.path路径依赖的详细说明
    - 补充了sys.path结构的具体内容介绍
    - 增加了模块查找路径的分析方法指导
    - 完善了import导入机制的工作原理说明

--- python-study/01_quick_start/09_import.py
+++ python-study/01_quick_start/09_import.py
@@ -1,5 +1,13 @@
 # import导入模块，用于导入其他文件中的代码
 # 简单来说，当我们要用别人写的代码，就需要通过import导入才能使用
+# import导入取决于sys.path路径
+# sys.path的结构是
+## 1. 当前工作目录（cd到的目录）
+## 2. 执行的文件所在目录
+## 3. 标准库
+## 4. ... 其他
+## 当你写from orm.utils import utils时，
+## 就会依次按照sys.path的路径去加载（{sys.path}/orm/utils）,如果当找不到模块时，要着重分析下sys.path和包名
 
 # 导入python标准库中的sys模块
 import sys
